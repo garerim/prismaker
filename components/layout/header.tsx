b33de495f1df5274506209394cba015c1f725b00
Matheo <garerimath73660@gmail.com>
2024-01-08 10:44:57 +0100

    add auth clrek system

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { ThemeToggle } from '../theme-toggle'
+import { UserButton } from "@clerk/nextjs";
 
 export const Header = () => {
   return (
-    <header className='w-full flex items-center px-3 py-2'>
+    <header className='w-full flex items-center px-3 py-2 gap-x-2'>
         <h1 className='text-3xl font-bold mr-auto'>Prismaker</h1>
+
+        <UserButton />
         <ThemeToggle />
     </header>
   )
